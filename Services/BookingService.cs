1ed5203ae3c1b299ea20f16ea73335940de1a72b
georgiachis3 <georgiachiswick@gmail.com>
2019-07-17 10:18:16 +0100

    Creation of Services, movement of code from Controller to Services

---
+++ Services/BookingService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+
+
+namespace Web.Data
+{
+    public class BookingService
+    {
+        public BookingContext Context { get; set; }
+
+        public Dictionary<int, TimeSpan> BookingLengthDictionary = new Dictionary<int, TimeSpan>();
+
+        public void CreateBooking(BookingContext context)
+        {
+            Context = context;
+
+            for (int i = 1; i <= 6; i++)
+            {
+                BookingLengthDictionary.Add(i, TimeSpan.FromMinutes(i * 30));
+            }
+
+        }
+    }          
+}
+
+    
+
